d32e01f4b573c47f7d68d59e0f0d73cc49ab85d8
Chris <36240719+KrisAsante@users.noreply.github.com>
2019-02-28 14:10:11 -0500

    Add files via upload
    Prints hello world to the console when the button is clicked

---
+++ hello world.py
@@ -0,0 +1,35 @@
+# Created by: Chris Asante
+# Created on: 08-Feb-2018
+# Created for: ICS3U
+# Daily Assignment  Unit 002
+# This program displays Hello World but with a menu to exit
+
+from tkinter import *
+
+class Application(Frame):
+    def say_hi(self):
+        print ("hi there, everyone!")
+
+    def createWidgets(self):
+        self.QUIT = Button(self)
+        self.QUIT["text"] = "QUIT"
+        self.QUIT["fg"]   = "red"
+        self.QUIT["command"] =  self.quit
+
+        self.QUIT.pack({"side": "left"})
+
+        self.hi_there = Button(self)
+        self.hi_there["text"] = "Hello",
+        self.hi_there["command"] = self.say_hi
+
+        self.hi_there.pack({"side": "left"})
+
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
+
+root = Tk()
+app = Application(master=root)
+app.mainloop()
+root.destroy()                         
